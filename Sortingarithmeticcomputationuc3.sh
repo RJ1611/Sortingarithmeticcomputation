d2392a3ee5497ec9ee54ae249d9d7d4abaf3d9b0
RJ1611 <rajasdhake1611@gmail.com>
2021-09-16 14:41:32 +0530

    created UC3

---
+++ Sortingarithmeticcomputationuc3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Welcome to Arithmetic Computation & Sorting Computation"
+echo -n "Enter 1st number"
+read firstInput
+echo -n "Enter 2nd number"
+read secondInput
+echo -n "Enter 3rd number"
+read thirdInput
+firstResult=`echo "$firstInput * $secondInput + $thirdInput" | bc`
+echo $firstResult
